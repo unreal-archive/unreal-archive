181bfbd8ec3ef6e3616226947bedc269557c3f98
Kenneth Watson <git@shrimpworks.za.net>
2018-10-04 19:49:13 +0200

    implement no-op data store for testing

--- src/net/shrimpworks/unreal/archive/storage/DataStore.java
+++ src/net/shrimpworks/unreal/archive/storage/DataStore.java
@@ -7,11 +7,12 @@ import java.util.function.Consumer;
 
 import net.shrimpworks.unreal.archive.CLI;
 
-public interface DataStore extends Closeable  {
+public interface DataStore extends Closeable {
 
 	enum StoreType {
 		DAV(new DavStore.Factory()),
 		B2(new B2Store.Factory()),
+		NOP(new NopStore.NopStoreFactory()),
 		;
 
 		private final DataStoreFactory factory;
@@ -42,4 +43,35 @@ public interface DataStore extends Closeable  {
 	public void delete(String url, Consumer<Boolean> deleted) throws IOException;
 
 	public void download(String url, Consumer<Path> downloaded) throws IOException;
+
+	static class NopStore implements DataStore {
+
+		static class NopStoreFactory implements DataStoreFactory {
+
+			@Override
+			public DataStore newStore(StoreContent type, CLI cli) {
+				return new NopStore();
+			}
+		}
+
+		@Override
+		public void store(Path path, String name, Consumer<String> stored) {
+			stored.accept("nop://" + name);
+		}
+
+		@Override
+		public void delete(String url, Consumer<Boolean> deleted) {
+			deleted.accept(true);
+		}
+
+		@Override
+		public void download(String url, Consumer<Path> downloaded) {
+			downloaded.accept(null);
+		}
+
+		@Override
+		public void close() {
+
+		}
+	}
 }
