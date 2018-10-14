6fe9cdc88bfbe58c5b87ec8efb4a1b7b222c611a
Kenneth Watson <git@shrimpworks.za.net>
2018-10-14 09:55:59 +0200

    placeholder for alternative gamefront thing

---
+++ src/net/shrimpworks/unreal/archive/scraper/GameFrontOnline.java
@@ -0,0 +1,9 @@
+package net.shrimpworks.unreal.archive.scraper;
+
+public class GameFrontOnline {
+
+	// this seems to have many more files than gamefront.com
+
+	// https://gamefront.online/files2/listing/pub2/unreal/
+	// https://gamefront.online/files2/listing/pub2/Unreal_Tournament/
+}
