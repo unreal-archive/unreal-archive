c470cc6970925fc3a599ef26562a4db19c6e1ff8
Kenneth Watson <git@shrimpworks.za.net>
2018-10-14 09:44:26 +0200

    add gametypes

--- src/net/shrimpworks/unreal/archive/indexer/maps/GameTypes.java
+++ src/net/shrimpworks/unreal/archive/indexer/maps/GameTypes.java
@@ -8,6 +8,7 @@ public class GameTypes {
 
 	private static final List<GameType> GAME_TYPES = Arrays.asList(
 			new GameType("Single Player", "SP"),
+			new GameType("1 on 1", "DM-1on1"),
 			new GameType("DeathMatch", "DM"),
 			new GameType("BunnyTrack", "CTF-BT", "BT"),
 			new GameType("Multi-Team CTF", "CTF4", "CTFM"),
@@ -23,12 +24,15 @@ public class GameTypes {
 			new GameType("Rocket Arena", "RA"),
 			new GameType("Jailbreak", "JB"),
 			new GameType("Tactical Ops", "TO"),
+			new GameType("Tactical Ops", "SW"),
 			new GameType("Infiltration", "INF"),
 			new GameType("UnWheel", "UW"),
 			new GameType("Thievery", "Thievery"),
 			new GameType("Unreal4Ever", "U4E"),
 			new GameType("Unreal Fortress", "UNF"),
-			new GameType("XMP", "XMP")
+			new GameType("XMP", "XMP"),
+			new GameType("FragBall", "FB"),
+			new GameType("Flag Domination", "FD")
 	);
 
 	public static GameType forMap(String mapName) {
