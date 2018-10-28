a22215c60ca791005b4cb3fa858e84fcefb6bcac
Kenneth Watson <git@shrimpworks.za.net>
2018-10-28 20:58:01 +0200

    placeholder/reminder unreal/uttexture scraper

---
+++ src/net/shrimpworks/unreal/archive/scraper/UTTexture.java
@@ -0,0 +1,15 @@
+package net.shrimpworks.unreal.archive.scraper;
+
+/**
+ * Valid as of 2018-10.
+ * <p>
+ * Unreal Tournament: http://www.uttexture.com/UT/Website/Downloads/Downloads.htm
+ * Unreal: http://www.unrealtexture.com/Unreal/Website/Downloads/Downloads.htm
+ * <p>
+ * Direct download listings:
+ * <p>
+ * Unreal Tournament: http://uttexture.com/UT/Downloads/
+ * Unreal: http://unrealtexture.com/Unreal/Downloads/
+ */
+public class UTTexture {
+}
