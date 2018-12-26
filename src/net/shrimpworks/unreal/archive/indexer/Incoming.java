48f832de3095c9b9cbf386fa82f496187e98a8bd
Kenneth Watson <git@shrimpworks.za.net>
2018-12-26 12:45:08 +0200

    detect and support ut2mod files

--- src/net/shrimpworks/unreal/archive/indexer/Incoming.java
+++ src/net/shrimpworks/unreal/archive/indexer/Incoming.java
@@ -42,7 +42,7 @@ public class Incoming implements Closeable {
 		PLAYER(true, "upl"),
 		INT(false, "int"),
 		INI(false, "ini"),
-		UMOD(true, "umod"),
+		UMOD(true, "umod", "ut2mod"),
 		TEXT(false, "txt"),
 		HTML(false, "html", "htm"),
 		IMAGE(false, "jpg", "jpeg", "bmp", "png", "gif"),
@@ -162,7 +162,7 @@ public class Incoming implements Closeable {
 			Files.walkFileTree(contentRoot, new SimpleFileVisitor<Path>() {
 				@Override
 				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-					if (file.toString().toLowerCase().endsWith(".umod")) {
+					if (file.toString().toLowerCase().endsWith(".umod") || file.toString().toLowerCase().endsWith(".ut2mod")) {
 						files.putAll(umodFiles(file));
 					}
 					files.put(file.toString(), file);
