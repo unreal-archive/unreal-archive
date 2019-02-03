2b9e7b99569f952b74f1171e788fc32654145f02
Kenneth Watson <git@shrimpworks.za.net>
2019-02-03 11:47:13 +0200

    tweak map pack classification

--- src/net/shrimpworks/unreal/archive/content/mappacks/MapPackClassifier.java
+++ src/net/shrimpworks/unreal/archive/content/mappacks/MapPackClassifier.java
@@ -11,23 +11,26 @@ public class MapPackClassifier implements Classifier {
 	@Override
 	public boolean classify(Incoming incoming) {
 		Set<Incoming.IncomingFile> maps = incoming.files(Incoming.FileType.MAP);
-		Set<Incoming.IncomingFile> codes = incoming.files(Incoming.FileType.CODE);
-		Set<Incoming.IncomingFile> ints = incoming.files(Incoming.FileType.INT);
+//		Set<Incoming.IncomingFile> codes = incoming.files(Incoming.FileType.CODE);
+		Set<Incoming.IncomingFile> disallowed = incoming.files(Incoming.FileType.INT,
+															   Incoming.FileType.INI,
+															   Incoming.FileType.PLAYER,
+															   Incoming.FileType.UCL);
 
 		// single map
 		if (maps.size() <= 1) return false;
 
-		// for now, we assume a map pack should only contain maps and textures and other media, not code
-		// presence of code package probably indicates a mod, rather than map pack
-		// int files likely also indicate some other sort of more specialised content
-		if (!codes.isEmpty() || !ints.isEmpty()) return false;
+		// we're disallowing things which may indicate a mod, mutator or other things, such
+		// as voices, models or skins. these things typically require an int or ucl file to
+		// define them, so if these are not present, we assume it is a bunch of maps, with
+		// some optional code packages (.u files, etc)
+		if (!disallowed.isEmpty()) return false;
 
 		// for now, we specifically disallow UT3, since we don't understand its package structure (> version 500?)
 		for (Incoming.IncomingFile map : maps) {
 			if (Util.extension(map.fileName()).equalsIgnoreCase("ut3")) return false;
 		}
 
-		// a bit naive, if there's a one-map mod, it would pass here
 		return true;
 	}
 }
