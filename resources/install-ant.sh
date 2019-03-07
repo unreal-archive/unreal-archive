9fbe226713cab1030dccecfb6eaf6d40eb578fef
Kenneth Watson <git@shrimpworks.za.net>
2019-03-07 19:28:30 +0200

    attempt to install custom ant version

---
+++ resources/install-ant.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ANT_VERSION=1.10.5
+
+ANT_URL="http://apache.saix.net/ant/binaries/apache-ant-${ANT_VERSION}-bin.zip"
+
+ANT_HOME=~/.ant/ant
+
+# download and install ant
+if [ ! -d "${ANT_HOME}/bin" ]; then
+  echo "Downloading ant binary from ${ANT_URL}"
+  curl "$ANT_URL" --output /tmp/ant-bin.zip
+  (cd /tmp && unzip ant-bin.zip && rm ant-bin)
+  mv "/tmp/apache-ant-${ANT_VERSION}" $ANT_HOME
+  (cd $ANT_HOME && ./bin/ant -f fetch.xml -Ddest=system)
+fi
