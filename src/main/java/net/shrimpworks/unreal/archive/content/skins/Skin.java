eff6e7fafaf8411fde0833d585386d95e9f93822
Kenneth Watson <git@shrimpworks.za.net>
2020-07-01 19:24:28 +0200

    tweak skin and face matching more

--- src/main/java/net/shrimpworks/unreal/archive/content/skins/Skin.java
+++ src/main/java/net/shrimpworks/unreal/archive/content/skins/Skin.java
@@ -15,9 +15,9 @@ public class Skin extends Content {
 	// Game/Type/A/
 	private static final String PATH_STRING = "%s/%s/%s/%s/";
 
-	static final Pattern NAME_MATCH = Pattern.compile(".+?\\.[a-zA-Z0-9]{4}\\d");
+	static final Pattern NAME_MATCH = Pattern.compile(".+?\\..{4}\\d");
 	static final Pattern NAME_MATCH_UNREAL = Pattern.compile(".+?\\.(.+?)");
-	static final Pattern FACE_MATCH = Pattern.compile(".+?\\.[a-zA-Z0-9]{4}\\d[a-zA-Z0-9]+");
+	static final Pattern FACE_MATCH = Pattern.compile(".+?\\..{4}\\d[a-zA-Z0-9]+");
 	static final Pattern FACE_PORTRAIT_MATCH = Pattern.compile("(.+?)\\.(.+?5[a-zA-Z0-9]+)"); // (something_lol).(word5name)
 	static final Pattern TEAM_MATCH = Pattern.compile(".+?\\..+?\\dT_\\d", Pattern.CASE_INSENSITIVE);
 
