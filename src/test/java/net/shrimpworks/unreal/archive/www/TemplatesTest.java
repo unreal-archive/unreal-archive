36a250a3abee850040c8da5252ffa5508f2fd1b3
Kenneth Watson <git@shrimpworks.za.net>
2020-12-06 19:25:35 +0200

    remove testing code

--- src/test/java/net/shrimpworks/unreal/archive/www/TemplatesTest.java
+++ src/test/java/net/shrimpworks/unreal/archive/www/TemplatesTest.java
@@ -1,16 +1,11 @@
 package net.shrimpworks.unreal.archive.www;
 
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import org.junit.jupiter.api.Test;
 
@@ -35,17 +30,4 @@ public class TemplatesTest {
 
 		assertTrue(foundCss[0]);
 	}
-
-	@Test
-	public void lol() throws IOException {
-		Document doc = Jsoup.parse(new URL("http://server/wiki/Legacy:ExtendedToolbar.html"), 1000);
-		doc.select("#mw-content-text #toc").remove();
-		doc.select("#mw-content-text .mw-editsection").remove();
-		for (Element e : doc.select("span.mw-headline")) {
-			e.parent().text(e.text());
-			//e.remove();
-		}
-		System.out.println(doc.selectFirst("#mw-content-text"));
-	}
-
 }
