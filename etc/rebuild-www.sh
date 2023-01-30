3a386070e090e361bd22e94ab3d0e1629535b770
Kenneth Watson <git@shrimpworks.za.net>
2023-01-30 09:06:53 +0800

    Test rebuild trigger

--- etc/rebuild-www.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Triggers a rebuild of the `unreal-archive-data` project
-
-echo "Triggering www content rebuild"
-
-curl -s -X POST \
-   -H "Authorization: Bearer ${DRONE_TOKEN}" \
-   https://build.shrimpworks.za.net/api/repos/unreal-archive/unreal-archive-data/builds
