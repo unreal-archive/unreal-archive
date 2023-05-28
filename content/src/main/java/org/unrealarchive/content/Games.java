c3a0ac52145c7f303395983f04e2db1dd9164b77
Kenneth Watson <git@shrimpworks.za.net>
2023-05-28 10:39:31 +0800

    add UT2003

--- content/src/main/java/org/unrealarchive/content/Games.java
+++ content/src/main/java/org/unrealarchive/content/Games.java
@@ -10,6 +10,8 @@ public enum Games {
 	UNREAL("Unreal", "Unreal", "Unreal", List.of("unreal", "u1", "gold")),
 	UNREAL_TOURNAMENT("Unreal Tournament", "UT99", "Unreal Tournament (UT99)", List.of("ut", "ut99", "unreal tournament", "goty")),
 	UNREAL_2("Unreal 2", "Unreal 2", "Unreal II", List.of("unreal 2", "u2", "uii", "xmp")),
+	UNREAL_TOURNAMENT_2003("Unreal Tournament 2003", "UT2003", "Unreal Tournament 2003 (UT2003)",
+						   List.of("ut2003", "ut2k3", "ut2003", "ut2")),
 	UNREAL_TOURNAMENT_2004("Unreal Tournament 2004", "UT2004", "Unreal Tournament 2004 (UT2004)",
 						   List.of("ut2004", "ut2k4", "ut2003", "ut2k3", "ece", "ut2")),
 	UNREAL_TOURNAMENT_3("Unreal Tournament 3", "UT3", "Unreal Tournament 3 (UT3)", List.of("ut3", "black")),
